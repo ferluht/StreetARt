a774ad9a83b32b9edf7602a54fd0234162650456
ferluht <ilya.belikov@phystech.edu>
2016-07-08 15:27:39 +0400

    interface template + second activity

--- app/src/main/java/eu/kudan/ar/MainActivity.java
+++ app/src/main/java/eu/kudan/ar/MainActivity.java
@@ -1,8 +1,10 @@
 package eu.kudan.ar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
 import eu.kudan.kudan.ARAPIKey;
 
 public class MainActivity extends AppCompatActivity
@@ -85,7 +89,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            Intent intent = new Intent(MainActivity.this, FullscreenAR.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
