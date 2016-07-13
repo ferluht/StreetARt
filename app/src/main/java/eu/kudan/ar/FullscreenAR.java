0b2a318aa0e6a349425d288c74fc0b0d336478be
ferluht <ilya.belikov@phystech.edu>
2016-07-13 12:15:08 +0400

    working 3d komus marker

--- app/src/main/java/eu/kudan/ar/FullscreenAR.java
+++ app/src/main/java/eu/kudan/ar/FullscreenAR.java
@@ -3,6 +3,8 @@ package eu.kudan.ar;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.jme3.math.Vector3f;
+
 import eu.kudan.kudan.*;
 
 /**
@@ -19,75 +21,36 @@ public class FullscreenAR extends ARActivity {
     }
 
     public void setup() {
-        ARImageTrackable trackable = new ARImageTrackable("test");
-        trackable.loadFromAsset("lego.jpg");
+        ARImageTrackable trackable = new ARImageTrackable("komus");
+        trackable.loadFromAsset("komus.jpg");
 
-<<<<<<< HEAD
         // Get instance of image tracker manager
         ARImageTracker trackableManager = ARImageTracker.getInstance();
-=======
-        // load a set of trackables from a bundled file.
-        //ARTrackableSet trackableSet = new ARTrackableSet();
-        //trackableSet.loadFromAsset("TV.KARMarker");
-
-        ARImageTrackable trackable = new ARImageTrackable("TV");
-        trackable.loadFromAsset("Markers/TV.jpg");
->>>>>>> origin/master
 
         // Add image trackable to image tracker manager
         trackableManager.addTrackable(trackable);
 
-<<<<<<< HEAD
         // Import model
         ARModelImporter modelImporter = new ARModelImporter();
-        modelImporter.loadFromAsset("testmodel.jet");
+        modelImporter.loadFromAsset("technostart.jet");
         ARModelNode modelNode = (ARModelNode)modelImporter.getNode();
 
         // Load model texture
         ARTexture2D texture2D = new ARTexture2D();
-        texture2D.loadFromAsset("texture.jpg");
+        texture2D.loadFromAsset("texture1.jpg");
 
-        // Apply model texture to model texture material
         ARLightMaterial material = new ARLightMaterial();
         material.setTexture(texture2D);
         material.setAmbient(0.8f, 0.8f, 0.8f);
+        // Apply model texture to model texture material
+        //ARColourMaterial material = new ARColourMaterial();
+        //material.setTexture(texture2D);
+        //material.setColour(new Vector3f(0.8f, 0.8f, 0.8f));
 
         // Apply texture material to models mesh nodes
         for(ARMeshNode meshNode : modelImporter.getMeshNodes()){
             meshNode.setMaterial(material);
         }
-=======
-        // add our trackables to the tracker.
-        //tracker.addTrackableSet(trackableSet);
-        tracker.addTrackable(trackable);
-
-        // create an image node. а картинку вмеоо обьекта можешь? это и есть картинка. ты имеешь в виде бэтмана? д
-        ARImageTrackable tvTrackable = tracker.findTrackable("TV");//tvTrackable == null
-        ARModelImporter importer = new ARModelImporter();
-        importer.loadFromAsset("output.jet");
-
-        ARModelNode imageNode = (ARModelNode) importer.getNode();
-        //ARImageNode imageNode = new ARImageNode("BatmanLegoMovie.png");
-
-        ARLightMaterial material = new ARLightMaterial();
-        material.setAmbient(0.8f, 0.8f, 0.8f);
-
-        for (ARMeshNode meshNode: importer.getMeshNodes()){
-            meshNode.setMaterial(material);
-        }
-
-        // make it smaller.
-        //imageNode.scaleBy(0.5f, 0.5f, 0.5f);
-
-        // add it to the lego trackable.
-        tvTrackable.getWorld().addChild(imageNode);
-    }
-
-    @Override
-    public void didDetect(ARImageTrackable trackable) {
-        Log.i("KudanSamples", "detected " + trackable.getName());
-    }
->>>>>>> origin/master
 
 
         modelNode.rotateByDegrees(90,1,0,0);
