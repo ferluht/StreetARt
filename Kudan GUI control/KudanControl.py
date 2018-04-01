b6fb0e6920e00a78add64b378ddef79a97456cb2
Ilya Belikov <ilya.belikov@phystech.edu>
2018-04-01 11:40:56 +0300

    working gui control with binaries

--- Kudan GUI control/KudanControl.py
+++ Kudan GUI control/KudanControl.py
@@ -4,18 +4,20 @@ from os import listdir, remove
 from os.path import isfile, join
 import win32api, win32con
 from pywinauto.timings import Timings
+import pywinauto.keyboard
+import pywinauto.mouse
 
 Timings.Slow()
 
-winProjectPath = 'D:\python_server\wincontrol'
+delay = 1
+
+winProjectPath = '.'
 rawFilesPath = 'D://YandexDisk/_uploads_temp'
 winRawFilesPath = 'D:\YandexDisk\_uploads_temp'
 winAssetsPath = 'D:\YandexDisk\_assets_ready'
 
 def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+    pywinauto.mouse.click(button='left', coords=(x, y))
 
 def addMarkerPress():
     click(350, 740)
@@ -63,85 +65,65 @@ def markerProjectOpenPress():
     click(240, 50)
 
 def kudanStartMaximize():
-    app = Application().start("KudanARToolkit.exe")
-    time.sleep(3)
-    print "Kudan started"
-    app.connect_(path = "Kudan AR Toolkit")
+    app = Application().start("KudanARToolkit\KudanARToolkit.exe")
+    time.sleep(delay*2)
+    print ("Kudan started")
+    # app.connect_(path = "Kudan AR Toolkit")
     KudanWindow = app.KudanARToolkit
     KudanWindow.Maximize()
+    return app
 
-def markerProjectOpen(projectPath, projectName):
-    app = Application()
-    app.connect_(path = "Kudan AR Toolkit")
+def markerProjectOpen(app, projectPath, projectName):
     markerProjectOpenPress()
     OpenWindow = app.Open
-    OpenWindow.Wait('ready')
-    OpenWindow.Edit.SetText(projectPath+'\\'+projectName)
-    OpenWindow.TypeKeys("{ENTER}")
+    time.sleep(delay)
+    prefix = ''
+    if projectPath != '.':
+        prefix = projectPath + '\\'
+    pywinauto.keyboard.SendKeys(prefix + projectName + "{ENTER}")
 
-def addMarker(markerPath, markerName):
+def addMarker(app, markerPath, markerName):
     addMarkerPress()
-    app = Application()
-    app.connect_(path = "Kudan AR Toolkit")
     OpenWindow = app.Open
-    OpenWindow.Wait('ready')
-    OpenWindow.Edit.SetText(markerPath+'\\'+markerName)
-    OpenWindow.TypeKeys("{ENTER}")
+    time.sleep(delay)
+    pywinauto.keyboard.SendKeys(markerPath+'\\'+markerName + "{ENTER}")
 
-def addModel(modelPath, modelName):
+def addModel(app, modelPath, modelName):
     openModelPress()
-    app = Application()
-    app.connect_(path = "Kudan AR Toolkit")
     OpenWindow = app.Open
-    OpenWindow.Wait('ready')
-    OpenWindow.Edit.SetText(modelPath+'\\'+modelName)
-    OpenWindow.TypeKeys("{ENTER}")
+    time.sleep(delay)
+    pywinauto.keyboard.SendKeys(modelPath+'\\'+modelName + "{ENTER}")
 
-def addAlpha(alphaPath, alphaName):
+def addAlpha(app, alphaPath, alphaName):
     openAlphaPress()
-    app = Application()
-    app.connect_(path = "Kudan AR Toolkit")
     OpenWindow = app.Open
-    OpenWindow.Wait('ready')
-    OpenWindow.Edit.SetText(alphaPath+'\\'+alphaName)
-    OpenWindow.TypeKeys("{ENTER}")
+    time.sleep(delay)
+    pywinauto.keyboard.SendKeys(alphaPath+'\\'+alphaName + "{ENTER}")
 
-def selectSave(modelPath, modelName):
+def selectSave(app, modelPath, modelName):
     selectSaveLocationPress()
-    app = Application()
-    app.connect_(path = "Kudan AR Toolkit")
     OpenWindow = app.Open
-    OpenWindow.Wait('ready')
-    OpenWindow.Edit.SetText(modelPath+'\\'+modelName)
-    OpenWindow.TypeKeys("{ENTER}")
+    time.sleep(delay)
+    pywinauto.keyboard.SendKeys(modelPath+'\\'+modelName + "{ENTER}")
 
-def exportMarker(markerPath, markerName):
+def exportMarker(app, markerPath, markerName):
     exportMarkerPress()
-    app = Application()
-    app.connect_(path = "Kudan AR Toolkit")
     SaveWindow = app.SaveAs
-    SaveWindow.Wait('ready')
-    SaveWindow.Edit.SetText(markerPath+'\\'+markerName)
-    SaveWindow.TypeKeys("{ENTER}")
+    time.sleep(delay)
+    pywinauto.keyboard.SendKeys(markerPath+'\\'+markerName + "{ENTER}")
 
-def exportModel(modelPath, modelName):
+def exportModel(app, modelPath, modelName):
     selectSaveLocationPress()
-    app = Application()
-    app.connect_(path = "Kudan AR Toolkit")
     SaveWindow = app.SaveAs
-    SaveWindow.Wait('ready')
-    SaveWindow.Edit.SetText(modelPath+'\\'+modelName)
-    SaveWindow.TypeKeys("{ENTER}")
+    time.sleep(delay)
+    pywinauto.keyboard.SendKeys(modelPath+'\\'+modelName + "{ENTER}")
     convertModelPress()
 
-def exportAlpha(alphaPath, alphaName):
+def exportAlpha(app, alphaPath, alphaName):
     saveAlphaPress()
-    app = Application()
-    app.connect_(path = "Kudan AR Toolkit")
     SaveWindow = app.SaveAs
-    SaveWindow.Wait('ready')
-    SaveWindow.Edit.SetText(alphaPath+'\\'+alphaName)
-    SaveWindow.TypeKeys("{ENTER}")
+    time.sleep(delay)
+    pywinauto.keyboard.SendKeys(alphaPath+'\\'+alphaName + "{ENTER}")
     convertAlphaPress()
 
 def removeMarker():
@@ -161,9 +143,8 @@ def alphaConverterOpen():
     alphaConverterPress()
     
 
-kudanStartMaximize()
-markerProjectOpen(winProjectPath, 'temp.ARProject')
-
+app = kudanStartMaximize()
+markerProjectOpen(app, winProjectPath, 'prj.ARProject')
 while(True):
     rawfiles = [f for f in listdir(rawFilesPath) if isfile(join(rawFilesPath, f))]
     if rawfiles:
@@ -172,27 +153,27 @@ while(True):
             extension = fil.split(".")[-1]
             if extension == "jpg":
                 markerConverterOpen()
-                addMarker(winRawFilesPath, fil)
-                exportMarker(winAssetsPath, fil.split(".")[0])
-                time.sleep(1)
+                addMarker(app, winRawFilesPath, fil)
+                exportMarker(app, winAssetsPath, fil.split(".")[0])
+                time.sleep(delay)
                 removeMarker()
                 remove(rawFilesPath + '/' + fil)
-                print fil + " generated"
+                print (fil + " generated")
 
             if extension == "png":
                 alphaConverterOpen()
                 for i in range(0,50):
-                    addAlpha(winRawFilesPath, fil)
-                exportAlpha(winAssetsPath, fil.split(".")[0])
-                time.sleep(5)
+                    addAlpha(app, winRawFilesPath, fil)
+                exportAlpha(app, winAssetsPath, fil.split(".")[0])
+                time.sleep(delay+5)
                 removeAlpha()
                 remove(rawFilesPath + '/' + fil)
-                print fil + " generated"
+                print (fil + " generated")
                 
             if extension == "fbx" or extension == "obj":
                 modelConverterOpen()
-                addModel(winRawFilesPath, fil)
-                exportModel(winAssetsPath, fil.split(".")[0])
-                time.sleep(5)
+                addModel(app, winRawFilesPath, fil)
+                exportModel(app, winAssetsPath, fil.split(".")[0])
+                time.sleep(delay+5)
                 remove(rawFilesPath + '/' + fil)
-                print fil + " generated"
+                print (fil + " generated")
